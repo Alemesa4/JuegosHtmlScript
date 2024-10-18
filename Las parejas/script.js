503b6467c2195330f9587b25a176e55b3c7e97d2
Alemesa4 <amenesess01@educarex.es>
2024-10-18 20:04:12 +0200

    Las parejas

---
+++ Las parejas/script.js
@@ -0,0 +1,13 @@
+const parejas=["🐕","🐕","🐈‍⬛","🐈‍⬛","🐎","🐎","🐸","🐸"];
+
+
+document.addEventListener("DOMContentLoaded",function(){
+    const bloque= document.getElementById("Bloque");
+
+parejas.forEach(pareja => {
+    const newDiv = document.createElement("div");
+
+    newItem.innerText = ${pareja};
+
+    list.appendChild(newItem);
+});
