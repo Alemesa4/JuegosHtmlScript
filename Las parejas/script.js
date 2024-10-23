fa9b698c168dc190f10f14f82f81f938b2f16b88
Alemesa4 <amenesess01@educarex.es>
2024-10-23 20:04:57 +0200

    Avance

--- Las parejas/script.js
+++ Las parejas/script.js
@@ -1,13 +1,40 @@
-const parejas=["🐕","🐕","🐈‍⬛","🐈‍⬛","🐎","🐎","🐸","🐸"];
+const parejas = ["🐕", "🐕", "🐈‍⬛", "🐈‍⬛", "🐎", "🐎", "🐸", "🐸"];
+const girados = [];
+const correctas = 0;
+parejas.sort(() => Math.random() - 0.5);
+document.addEventListener("DOMContentLoaded", function () {
+    const bloque = document.getElementById("Bloque");
 
+    parejas.forEach(pareja => {
+        const newDiv = document.createElement("div");
 
-document.addEventListener("DOMContentLoaded",function(){
-    const bloque= document.getElementById("Bloque");
+        newDiv.classList.add("animal");
+        newDiv.setAttribute("Icono", pareja);
+        newDiv.addEventListener('click', girar);
 
-parejas.forEach(pareja => {
-    const newDiv = document.createElement("div");
+        bloque.appendChild(newDiv);
+    })
+}
+);
 
-    newItem.innerText = ${pareja};
+function girar() {
+    const icono = this.getAttribute("Icono");
+    girados.push(icono);
+    this.innerText = icono;
+    console.log(girados);
+    if (girados.length == 2) {
+        if (girados[0] == girados[1]) {
+            correctas++;
+            girados.length = 0;
+        } else {
+            const animales = document.querySelectorAll(".animal");
+            animales.forEach(animal => {
+                if (animal.innerText === girados[0] || animal.innerText === girados[1]) {
+                    animal.innerText = "";
+                }
+            });
+            girados.length= 0;
+        }
+    }
 
-    list.appendChild(newItem);
-});
+}
