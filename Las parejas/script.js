e64d06e525f19ad0430262299d4722325cb03c49
Alejandro Meneses Sánchez <amenesess01@iesagora.caceres>
2024-10-24 19:22:31 +0200

    Juego terminado

--- Las parejas/script.js
+++ Las parejas/script.js
@@ -1,40 +1,51 @@
 const parejas = ["🐕", "🐕", "🐈‍⬛", "🐈‍⬛", "🐎", "🐎", "🐸", "🐸"];
 const girados = [];
-const correctas = 0;
+let correctas = 0;
 parejas.sort(() => Math.random() - 0.5);
 document.addEventListener("DOMContentLoaded", function () {
     const bloque = document.getElementById("Bloque");
 
     parejas.forEach(pareja => {
         const newDiv = document.createElement("div");
-
+        const resultado = document.getElementById("resultado");
+        
         newDiv.classList.add("animal");
         newDiv.setAttribute("Icono", pareja);
         newDiv.addEventListener('click', girar);
 
         bloque.appendChild(newDiv);
     })
+    
 }
 );
 
 function girar() {
-    const icono = this.getAttribute("Icono");
-    girados.push(icono);
-    this.innerText = icono;
-    console.log(girados);
-    if (girados.length == 2) {
-        if (girados[0] == girados[1]) {
-            correctas++;
-            girados.length = 0;
-        } else {
-            const animales = document.querySelectorAll(".animal");
-            animales.forEach(animal => {
-                if (animal.innerText === girados[0] || animal.innerText === girados[1]) {
-                    animal.innerText = "";
-                }
-            });
-            girados.length= 0;
-        }
-    }
 
-}
+    const icono = this.getAttribute("Icono");
+    girados.push(icono); 
+    this.innerText = icono;
+
+    console.log(girados);
+    console.log(correctas);
+    if (girados.length === 2) {
+
+        if (girados[0] === girados[1]) {
+            correctas++;
+            girados.length = 0; 
+            if(correctas == 4) {
+                resultado.innerText= "Has ganado";
+            }
+        } else {
+            setTimeout(() => {
+                const animales = document.querySelectorAll(".animal");
+                animales.forEach(animal => {
+                    if (animal.innerText === girados[0] || animal.innerText === girados[1]) {
+                        animal.innerText = ""; 
+                    }
+                });
+
+                girados.length = 0; 
+               
+            }, 1000); 
+        }}};
+
